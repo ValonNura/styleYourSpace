d1df3a319e870c2f0a46c6ed1d7db77cf43732fb
anitarexhepi <anitarxh@gmail.com>
2024-11-27 00:00:39 +0100

    Ngjyrat edhe emrat e qyteteve

--- script.js
+++ script.js
@@ -55,3 +55,12 @@ function validateSignup() {
     alert("Signup successful!");
     return true;
 }
+
+function toggleMenu() {
+    
+    if (window.innerWidth <= 768) {
+      const navLinks = document.getElementById("navLinks");
+      navLinks.classList.toggle("active"); 
+    }
+  }
+  
